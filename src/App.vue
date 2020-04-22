feae75580035c1940438ee7f69f50ddc2a415455
jianghan <jianghan@tp-link.com.cn>
2020-04-22 08:52:13 +0800

    添加测试修改

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,10 @@
 <style>
     @import "../static/css/main.css";
     @import "../static/css/color-dark.css";     /*深色主题*/
-    /*@import "../static/css/theme-green/color-green.css";   浅绿色主题*/
+    /*@import "../static/css/theme-green/color-green.css";   测试主题*/
+	/*@import "../static/css/theme-green/color-green.css";   测试主题*/
+	/*@import "../static/css/theme-green/color-green.css";   测试主题*/
+	/*@import "../static/css/theme-green/color-green.css";   测试主题*/
+	/*@import "../static/css/theme-green/color-green.css";   测试主题*/
+	/*@import "../static/css/theme-green/color-green.css";   测试主题*/
 </style>
